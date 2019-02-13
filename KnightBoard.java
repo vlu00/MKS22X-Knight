f3a4f41d93546cf1a0e5cb7d1f0bb919abb2dcd1
vlu00 <vlu00@stuy.edu>
2019-02-13 14:46:54 -0500

    Added exceptions for solve and countSolutions'

--- KnightBoard.java
+++ KnightBoard.java
@@ -7,16 +7,14 @@ public class KnightBoard {
     if (startingRows <= 0 || startingCols <= 0) {
       throw new IllegalArgumentException();
     }
-    else {
-      board = new int[startingRows][startingCols];
-      for (int r = 0; r < startingRows; r++) {
-        for (int c = 0; c < startingCols; c++) {
-          board[r][c] = 0;
-        }
-      }
-    }
     rows = startingRows;
     cols = startingCols;
+    board = new int[rows][cols];
+    for (int r = 0; r < startingRows; r++) {
+      for (int c = 0; c < startingCols; c++) {
+        board[r][c] = 0;
+      }
+    }
   }
 
   public String toString() {
@@ -43,9 +41,41 @@ public class KnightBoard {
     return display;
   }
 
+  public boolean isException() {
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < cols; c++) {
+        if (board[r][c] == 0) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public boolean solve(int startingRow, int startingCol){
+    if (isException()) {
+      throw new IllegalStateException();
+    }
+    if (startingRow <= 0 || startingCol <= 0) {
+      throw new IllegalArgumentException();
+    }
+
+  }
+  
+/*
+  public int countSolutions(int startingRow, int startingCol){
+    if (isException()) {
+      throw new IllegalStateException();
+    }
+    if (startingRows <= 0 || startingCols <= 0) {
+      throw new IllegalArgumentException();
+    }
+
+  }
+*/
   public static void main(String[] args) {
     KnightBoard A = new KnightBoard(3, 3);
-    System.out.println(A.toString()); 
+    System.out.println(A.toString());
   }
 
 }
