e996312b8ec13b9e15cd80f70e87b3462fec17ae
vlu00 <vlu00@stuy.edu>
2019-02-27 02:11:59 -0500

    Fixing format

--- KnightBoard.java
+++ KnightBoard.java
@@ -240,44 +240,4 @@ public class KnightBoard {
     }
     return solutions;
   }
-<<<<<<< HEAD
-=======
-
-  //testcase must be a valid index of your input/output array
-public static void runTest(int i){
-
-  KnightBoard b;
-  int[]m =   {4,5,5,5,5};
-  int[]n =   {4,5,4,5,5};
-  int[]startx = {0,0,0,1,2};
-  int[]starty = {0,0,0,1,2};
-  int[]answers = {0,304,32,56,64};
-  if(i >= 0 ){
-    try{
-      int correct = answers[i];
-      b = new KnightBoard(m[i%m.length],n[i%m.length]);
-
-      int ans  = b.countSolutions(startx[i],starty[i]);
-
-      if(correct==ans){
-        System.out.println("PASS board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans);
-      }else{
-        System.out.println("FAIL board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans+" vs "+correct);
-      }
-    }catch(Exception e){
-      System.out.println("FAIL Exception case: "+i);
-
-    }
-  }
-}
-
-  public static void main(String[] args) {
-    runTest(0);
-    runTest(1);
-    runTest(2);
-    runTest(3);
-    runTest(4);  
-  }
-
->>>>>>> d6bf22767d3724d5cd6958d3fad0bcdabcd1495f
 }
